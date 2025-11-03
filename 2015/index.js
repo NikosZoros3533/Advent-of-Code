0584be8cf68610e9affdfd86c9c799475a42f3c4
NikosZoros3533 <nikozoro@csd.auth.gr>
2025-11-04 00:09:21 +0200

    solution:138

--- 2015/index.js
+++ 2015/index.js
@@ -1,1 +1,20 @@
 //Advent of Code 2015 Solution
+const fs = require("fs");
+let inputData;
+try {
+  const data = fs.readFileSync("input.txt", "utf8");
+  inputData = data;
+} catch (err) {
+  console.error("Error reading file:", err);
+}
+let text = "" + inputData;
+let solution = 0;
+const textArray = Array.from(text);
+textArray.forEach((c) => {
+  if (c == "(") {
+    solution++;
+  }if(c == ")") {
+    solution--;
+  }
+});
+console.log(solution);
